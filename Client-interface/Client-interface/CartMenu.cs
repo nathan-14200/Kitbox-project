0460a24d3be0f31ec48b9b98509f1e59aaf13912
nathan-14200 <nathan_3145@hotmail.com>
2018-02-21 10:23:15 +0100

    first 2 windows
    created Home and Cart Menu window

---
+++ Client-interface/Client-interface/CartMenu.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Client_interface
+{
+    public partial class CartMenu : Form
+    {
+        public CartMenu()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cancel_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
