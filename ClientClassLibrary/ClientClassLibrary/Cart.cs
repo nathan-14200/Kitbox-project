7a52e3d4de12286786225484e7b158205eacca17
nathan <nathan@hashcode.com>
2018-03-07 11:31:12 +0100

    Creating basic classes

---
+++ ClientClassLibrary/ClientClassLibrary/Cart.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientClassLibrary
+{
+    class Cart
+    {
+        private List<Cabinet> stock = new List<Cabinet>();
+
+
+        public static List<Piece> TotalComposing()
+        {
+
+        }
+    }
+}
---
+++ ClientClassLibrary/ClientClassLibrary/Piece.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientClassLibrary
+{
+    class Piece
+    {
+        private int ID;
+        private string name;
+        private float price;
+        private string colour;
+        private bool instock;
+
+        public Piece(int ID, string name, float price, string colour, bool instock)
+        {
+            this.ID = ID;
+            this.name = name;
+            this.price = price;
+            this.colour = colour;
+            this.instock = instock;
+        }
+
+
+        public Boolean IsInStock()
+        {
+            return this.instock;
+        }
+
+        public string GetName()
+        {
+            return this.name;
+        }
+
+        public float GetPrice()
+        {
+            return this.price;
+        }
+
+        public string GetColour()
+        {
+            return this.colour;
+        }
+
+        public int GetID()
+        {
+            return this.ID;
+        }
+    }
+}
