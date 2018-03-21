b79aa21c286407c020699041e76585c09b1e8c5a
2a6m <maxime.bourguignon1996@gmail.com>
2018-03-21 10:49:08 +0100

    create class connection to DB

--- ClientClassLibrary/ClientClassLibrary/InitComp.cs
+++ ClientClassLibrary/ClientClassLibrary/InitComp.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -19,6 +20,37 @@ namespace ClientClassLibrary
         public static List<Piece> GetAllPieces()
         {
             return allPieces;
+        }
+
+        public static List<Object[]> ReadDB(String CmdString)
+        {
+            String ConnectionString = String.Format("Server={0};Database={1};Trusted_Connection={2}", "server_name", "database_name", "bool");
+            String CommandString = String.Format("SELECT {0} FROM {1} WHERE {2}", "*", "table_name", "condition");
+            List<Object[]> Result = new List<Object[]>();
+
+            using (SqlConnection conn = new SqlConnection())
+            {
+                conn.ConnectionString = ConnectionString;
+                conn.Open();
+
+                // use the connection here
+                SqlCommand command = new SqlCommand(CmdString, conn);
+
+                using(SqlDataReader reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        List<String> Data = new List<string>();
+
+                        Object[] values = new Object[reader.FieldCount];
+                        int fieldCount = reader.GetValues(values);
+
+                        Result.Add(values);
+                    }
+                }
+            }
+
+            return Result;
         }
     }
 }
