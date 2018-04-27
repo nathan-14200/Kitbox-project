ab31f8089f146993b98858f1be58015c3b737e46
nathan <nathan@hashcode.com>
2018-04-27 15:52:47 +0200

    creating session class

---
+++ Client-interface/Client-interface/Session.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClientClassLibrary;
+
+namespace Client_interface
+{
+    public class Session
+    {
+        private float selected_width = 0; 
+        private float selected_detpth = 0;
+        private Cart myCart;
+    }
+}
