68031fab440ba6a4c235066721a98c74582908b9
Maxime <maxime.bourguignon1996@gmail.com>
2018-05-01 12:05:07 +0200

    init commit

---
+++ StoreKeeper/StoreKeeper/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace StoreKeeper
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
