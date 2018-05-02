1d8cdbfdf8794e0592af9c2027a623e0fce8456a
Maxime <maxime.bourguignon1996@gmail.com>
2018-05-02 21:16:17 +0200

    add Client class Library

---
+++ ClientClassLibrary/ClientClassLibrary/Box.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientClassLibrary
+{
+    public class Box
+    {
+        private float height;
+        private float width;
+        private float depth;
+        private string colour;
+        private bool HasAdder;
+        private Dictionary<Piece, int> composition = new Dictionary<Piece, int>();
+        private string adder;
+
+
+        public Box(float height, float width, float depth, string colour, bool hasAdder)
+        {
+            //The typical pieces of a box must be added here in composition
+            this.height = height;
+            this.width = width;
+            this.depth = depth;
+            this.colour = colour;
+            this.HasAdder = hasAdder;
+            this.composition = SetBox(colour, height, width, depth);
+        }
+
+        public Dictionary<Piece, int> GetComposition()
+        {
+            // Has to add the minimal pieces with the ones of the Adder
+            Dictionary<Piece, int> comp = this.composition;
+
+            Dictionary<Piece, int> adderComp = Adder.GetPieces(adder);
+            foreach(var item in adderComp)
+            {
+                comp.Add(item.Key, item.Value);
+            }
+            return comp;
+        }
+
+        public void SetAdder(string adder)
+        {
+            //Check if adder in our list before accepting a change  
+            List<string> possibleAdder = Adder.GetAdder();
+            if (possibleAdder.Contains(adder))
+            {
+                this.adder = adder;
+            }                
+        }
+
+        public float getHeight()
+        {
+            //Adding 2 * 2cm of the 2 traverse
+            return height + 4;
+
+        }
+
+
+        private Dictionary<Piece, int> SetBox(string colour, float height, float width, float depth)
+        {
+            Dictionary<Piece, int> myBox = new Dictionary<Piece, int>();
+            List<Piece> allPieces = InitComp.GetAllPieces();
+            
+            
+            foreach(Piece piece in allPieces)
+            {
+                if(piece.GetName() == "Panneau Ar" && piece.GetColour() == colour && piece.GetHeight() == height && !myBox.ContainsKey(piece))
+                {
+                    myBox[piece] = 1;
+                }
+                else if(piece.GetName() == "Panneau GD" && piece.GetColour() == colour && piece.GetHeight() == height && !myBox.ContainsKey(piece))
+                {
+                    myBox[piece] = 2;
+                }
+                else if (piece.GetName() == "Panneau HB" && piece.GetColour() == colour && piece.GetHeight() == height && !myBox.ContainsKey(piece))
+                {
+                    //2 HB panels
+                    myBox[piece] = 2;
+                }//Only one type of traverse differing by their height
+                else if (piece.GetName() == "Traverse Ar" && piece.GetColour() == colour && piece.GetWidth() == height)
+                {
+                    //2  AR traverses
+                    myBox[piece] = 2;
+                }
+                else if (piece.GetName() == "Traverse Av" && piece.GetColour() == colour && piece.GetHeight() == height)
+                {
+                    //2 Av traverse
+                    myBox[piece] = 2;
+                }
+                else if (piece.GetName() == "Traverse GD" && piece.GetColour() == colour && piece.GetHeight() == height)
+                {
+                    //4 GD traverse
+                    myBox[piece] = 4;
+                }
+                else if(piece.GetName() == "Tasseau" && piece.GetHeight() == height)
+                {
+                    //4 Tasseau
+                    myBox[piece] = 4;
+                }
+            }
+
+            return myBox;
+        }
+
+
+        public double GetPrice()
+        {
+            double price = 0;
+
+            foreach (var item in this.composition)
+            {
+                price += item.Key.GetPrice() * item.Value;
+            }
+
+            return price;
+        }
+
+    }
+}
---
+++ ClientClassLibrary/ClientClassLibrary/Cabinet.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientClassLibrary
+{
+    public class Cabinet
+    {
+        private List<Box> boxComposition;
+        private float width;
+        private float depth;
+        private string id;
+        //value of the heighest Corniere
+        private float maxHeight = 375;
+        //private double height;
+        private List<Piece> composition;
+
+
+        public Cabinet(float width, float depth)
+        {
+            this.width = width;
+            this.depth = depth;
+            
+            
+        }
+
+        public float GetWidth()
+        {
+            return this.width;
+        }
+
+        public float GetDepth()
+        {
+            return this.depth;
+        }
+
+
+        public float height
+        {
+            get { return this.height; }
+            set { this.height = value; }
+        }
+
+        public int AddBox(float h, float width, float depth, string colour, bool hasAdder)
+        {
+            //Has to check if not more than 7 boxes and certain height limit.
+            //Error message if not added?
+            
+            float height = h + 4 + this.height;
+            if(boxComposition.Count < 7 && maxHeight > height)  // + height limit
+            {
+                boxComposition.Add(new Box(height, width, depth, colour, hasAdder));
+                this.height += boxComposition[boxComposition.Count - 1].getHeight();
+
+                return 0;
+            }
+
+            return 1;            
+        }
+
+        public void DeleteBox(Box box)
+        {
+            //To be changed (no print)
+
+            foreach (Box elem in this.boxComposition)
+            {
+                if (box == elem)
+                {
+                    boxComposition.Remove(box);
+                }
+                else
+                {
+                    Console.WriteLine("Box not found");
+                }
+            }
+        }
+
+
+        public List<Piece> GetComposition()
+        {
+            List<Piece> comp = composition;
+
+            foreach(Box box in boxComposition)
+            {
+                comp.AddRange(box.GetComposition());
+            }
+            return comp;
+        }
+
+
+
+        public float GetPrice()
+        {
+            float price = 0;
+            List<Piece> comp = GetComposition();
+  
+            foreach(Piece piece in comp)
+            {
+                price += piece.GetPrice();
+            }
+            return price;
+        }
+    }
+}
