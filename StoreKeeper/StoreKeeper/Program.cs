3b1124172159c3658cc25c5dd618206bca21041a
2a6m <maxime.bourguignon1996@gmail.com>
2018-05-31 11:46:15 +0200

    Create Menu

--- StoreKeeper/StoreKeeper/Program.cs
+++ StoreKeeper/StoreKeeper/Program.cs
@@ -1,12 +1,40 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace StoreKeeper
 {
     class MainClass
     {
+        private static List<string> menuItems = new List<string>()
+            {
+                "a",
+                "b",
+                "Exit"
+            };
+
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<string> menuText = new List<string>();
+            for(int i = 0; i < menuItems.Count; i++)
+            {
+                menuText.Add(String.Format("[{0}]    {1}", i, menuItems[i]));
+            }
+
+            string choice = Menuprint(menuText);
+        }
+
+        public static string Menuprint(List<string> menu)
+        {
+            foreach(string item in menu)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.Write("Your Choice : ");
+            string userChoice = Console.ReadLine();
+            Console.Clear();
+
+            return userChoice;
         }
     }
 }
