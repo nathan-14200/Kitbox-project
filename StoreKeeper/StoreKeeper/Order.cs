82b9726d40f05d619e672d2f7ef51b4213d5669d
2a6m <maxime.bourguignon1996@gmail.com>
2018-06-01 10:50:19 +0200

    Add ToString() to Order

--- StoreKeeper/StoreKeeper/Order.cs
+++ StoreKeeper/StoreKeeper/Order.cs
@@ -19,7 +19,7 @@ namespace StoreKeeper
             // Client = {"Complete": "yes/no", "telephone number": "02***", "address": "Promenade de l'Alma 5"}
         }
 
-        private bool CheckComplete()
+        public bool IsComplete()
         {
             foreach (Item item in this.Items.Values)
             {
@@ -30,6 +30,31 @@ namespace StoreKeeper
             }
 
             return true;
+        }
+
+        public List<Item> IncompleteList()
+        {
+            List<Item> list = new List<Item>();
+            foreach(Item item in this.Items.Values)
+            {
+                if (item.Complete == 0)
+                {
+                    list.Add(item);
+                }
+            }
+
+            return list;
+        }
+
+        public override string ToString()
+        {
+            String str = "";
+            // Add information about the Client
+            foreach (String key in this.Items.Keys)
+            {
+                str += String.Format("{0} : {1}{2}", key, this.Items[key].ToString(), Environment.NewLine);
+            }
+            return str;
         }
     }
 }
