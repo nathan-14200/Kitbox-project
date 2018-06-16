2ddc1b4eed07fa0c0e8e61e87192b5e26d7f56a3
2a6m <maxime.bourguignon1996@gmail.com>
2018-06-16 15:27:45 +0200

    add class piece for stock

---
+++ StoreKeeper/StoreKeeper/Piece.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StoreKeeper
+{
+    class Piece
+    {
+        private string code;
+        private int quantity;
+        private int quantity_min;
+
+        public Piece(string Code, int Quantity, int mQuantity)
+        {
+            this.code = Code;
+            this.quantity = Quantity;
+            this.quantity_min = mQuantity;
+        }
+
+        public int Quantity
+        {
+            get { return this.quantity; }
+            set { this.quantity = this.quantity + value; }
+        }
+
+        public int MQuantity
+        {
+            get { return this.quantity_min; }
+        }
+
+        public string Code
+        {
+            get { return this.code; }
+        }
+    }
+}
